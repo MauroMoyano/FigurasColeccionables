567597222bf217f21b62ec42ddc116cc0cc53378
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-04-15 21:39:12 -0300

    index + mongodb ok

--- src/pages/api/mongodb.js
+++ src/pages/api/mongodb.js
@@ -7,7 +7,8 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 export default async function handler(req, res) {
     if (!client.isConnected()) await client.connect();
-    req.db = client.db('nombre-de-tu-base-de-datos');
+    req.db = client.db('Figuras');
     req.client = client;
+    console.log(req.db+ "rfrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
     return handler(req, res);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,45 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from "next/head";
+import { NextApiRequest } from 'next';
+import { Db, MongoClient } from 'mongodb';
+import { GetServerSideProps } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Head>
         <title>Figuras Coleccionables</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        <h1>Data from MongoDB:</h1>
+        <ul>
+            {data.map((item) => (
+                <li key={item._id}>{item.Nombre}</li>
+            ))}
+        </ul>
     </main>
   )
 }
+
+type Props = {
+    data: Record<string, unknown>[];
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+    const req = context.req as NextApiRequest & { db: Db };
+    const client = new MongoClient(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    await client.connect();
+    req.db = client.db('Figuras');
+    const data = await req.db.collection('Productos').find().toArray();
+    return {
+        props: {
+            data: JSON.parse(JSON.stringify(data)),
+        },
+    };
+};
